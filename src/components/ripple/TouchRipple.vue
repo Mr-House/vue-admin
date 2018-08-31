26588463a3766497f7ea7c593a64165b614287ec
liutao <vaceliu@gmail.com>
2018-08-31 18:13:48 +0800

    波纹动画效果

---
+++ src/components/ripple/TouchRipple.vue
@@ -0,0 +1,17 @@
+<template>
+    <div @mousedown="handleMouseDown" @mouseup="end" @mouseleave="end" @touchstart="handleTouchStart" @touchend="end" @touchcancel="end">
+        <div :style="style" ref="holder">
+            <circle-ripple v-for="ripple in ripples" :key="ripple.key" :color="ripple.color" :opacity="ripple.opacity" :merge-style="ripple.style"></circle-ripple>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+<script>
+import CircleRipple from './CircleRipple'
+export default {
+    components: {
+        'circle-ripple': CircleRipple
+    }
+}
+</script>
+
